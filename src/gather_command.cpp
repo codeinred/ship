4a7f9ac473197ad24bfcac0733016e98e08b7e87
functionalperez <perez.cs@pm.me>
2020-09-12 19:27:20 -0600

    Added graph command

---
+++ src/gather_command.cpp
@@ -0,0 +1,43 @@
+#include <chrono>
+#include <cstdlib>
+#include <fmt/chrono.h>
+#include <fmt/core.h>
+#include <ship/ship.h>
+#include <unistd.h>
+
+using namespace ship;
+
+int gather_command(int argc, char** argv, char** env) {
+    std::vector<fs::path> files;
+    for (int i = 2; i < argc; i++) {
+        files.push_back(argv[i]);
+    }
+    bool any_missing = false;
+    for (auto& path : files) {
+        if (!fs::exists(path)) {
+            fmt::print("Cannot find file: {}\n", path.string());
+            any_missing = true;
+        }
+    }
+    if (any_missing) {
+        return 1;
+    }
+
+    fs::path working_dir = fmt::format(
+        "/tmp/ship-{}-{}/", getpid(),
+        std::chrono::high_resolution_clock::now().time_since_epoch());
+
+    if (!fs::create_directory(working_dir)) {
+        fmt::print("Error: Unable to create working directory at {}\n",
+                   working_dir.string());
+        return 1;
+    }
+
+    fs::current_path(working_dir);
+
+    auto graph = io::get_dependency_graph(files);
+
+
+
+    return 0;
+}
